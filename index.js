f146f69bd25c99c507cca5d352b87f5b05de3465
Tu Linh <linhtunguyen.1311@gmail.com>
2021-01-30 00:08:57 +0700

    add map

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 var gameState = {};
+var playerDirection;
+var spaceBar;
 var config = {
     type: Phaser.AUTO,
     width: 800,
@@ -6,76 +8,138 @@ var config = {
     scene: {
         preload: preload,
         create: create,
-        update: update
+        update: update,
+    },
+    scale: {
+        zoom: 1.25
     }
 };
-
+var number = 64;
 var game = new Phaser.Game(config);
 
-function preload ()
-{
+function preload() {
     // background image 
     this.load.image('sky', 'https://labs.phaser.io/assets/skies/starfield.png');
+    this.load.image('tiles', 'resource/0x72_DungeonTilesetII_v1.3.1/0x72_DungeonTilesetII_v1.3.png');
+    this.load.tilemapTiledJSON('dungeon', 'resource/dungeon.json')
     // player
-    this.load.spritesheet('player', 'resource/spritesheet.png', { frameWidth: 64, frameHeight: 64 })
+    this.load.spritesheet('player', 'resource/spritesheet.png', { frameWidth: number, frameHeight: number })
+    this.load.spritesheet('slash', 'resource/slash-sprite.png', { frameWidth: number * 3, frameHeight: number * 3 })
 }
 
-function create ()
-{
-    this.add.image(400, 300, 'sky');
-    gameState.player = this.add.sprite(400, 300, 'player');
+function create() {
+    this.add.image(200, 150, 'sky');
+    const map = this.make.tilemap({ key: 'dungeon'})
+    const tileset = map.addTilesetImage('0x72_DungeonTilesetII_v1.3', 'tiles')
+    map.createLayer(0, tileset, 0, 0)
+    map.createLayer(1, tileset, 0, 0)
+    map.createLayer(2, tileset, 0, 0)
+    map.createLayer(3, tileset, 0, 0)
+    gameState.player = this.add.sprite(200, 150, 'player');
 
-    this.anims.create({
+    gameState.player.anims.create({
         key: 'up',
         frames: this.anims.generateFrameNumbers('player', { start: 192, end: 200 }),
         frameRate: 10,
         repeat: -1
     });
-    this.anims.create({
+    gameState.player.anims.create({
         key: 'left',
         frames: this.anims.generateFrameNumbers('player', { start: 216, end: 224 }),
         frameRate: 10,
         repeat: -1
     });
-    this.anims.create({
+    gameState.player.anims.create({
         key: 'down',
         frames: this.anims.generateFrameNumbers('player', { start: 240, end: 248 }),
         frameRate: 10,
         repeat: -1
     });
-    this.anims.create({
+    gameState.player.anims.create({
         key: 'right',
         frames: this.anims.generateFrameNumbers('player', { start: 264, end: 272 }),
         frameRate: 10,
         repeat: -1
     });
-    this.anims.create({
-        key: 'nonw',
-        frames: this.anims.generateFrameNumbers('player', { frame: 18 }),
-        frameRate: 20,
+
+    gameState.player.anims.create({
+        key: 'slash-up',
+        frames: this.anims.generateFrameNumbers('slash', { start: 0, end: 5 }),
+        frameRate: 10,
+        repeat: 1
+    });
+    gameState.player.anims.create({
+        key: 'slash-left',
+        frames: this.anims.generateFrameNumbers('slash', { start: 6, end: 11 }),
+        frameRate: 10,
+        repeat: 1
+    });
+    gameState.player.anims.create({
+        key: 'slash-down',
+        frames: this.anims.generateFrameNumbers('slash', { start: 12, end: 17 }),
+        frameRate: 10,
+        repeat: 1
+    });
+    gameState.player.anims.create({
+        key: 'slash-right',
+        frames: this.anims.generateFrameNumbers('slash', { start: 18, end: 23 }),
+        frameRate: 10,
+        repeat: 1
     });
 
     gameState.cursors = this.input.keyboard.createCursorKeys();
+    // spaceBar = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+};
+function slash() {
+    if (playerDirection == 'right') {
+        // gameState.player.setTexture('slash', 18)
+        gameState.player.anims.play('slash-right', true);
+    }
+    else if (playerDirection == 'left') {
+        // gameState.player.setTexture('slash', 6)
+        gameState.player.anims.play('slash-left', true);
+    }
+    else if (playerDirection == 'up') {
+        // gameState.player.setTexture('slash', 0)
+        gameState.player.anims.play('slash-up', true);
+    }
+    else if (playerDirection == 'down') {
+        // gameState.player.setTexture('slash', 12)
+        gameState.player.anims.play('slash-down', true);
+    }
 }
-
 function update() {
     if (gameState.cursors.right.isDown) {
         gameState.player.x += 2
         gameState.player.anims.play('right', true);
-      }
+        playerDirection = 'right'
+
+    }
     else if (gameState.cursors.left.isDown) {
-    gameState.player.x -= 2
-    gameState.player.anims.play('left', true);
+        gameState.player.x -= 2
+        gameState.player.anims.play('left', true);
+        playerDirection = 'left'
+
     }
     else if (gameState.cursors.up.isDown) {
-    gameState.player.y -= 2;
-    gameState.player.anims.play('up', true);
+        gameState.player.y -= 2;
+        gameState.player.anims.play('up', true);
+        playerDirection = 'up'
+
     }
     else if (gameState.cursors.down.isDown) {
-    gameState.player.y += 2;
-    gameState.player.anims.play('down', true);
+        gameState.player.y += 2;
+        gameState.player.anims.play('down', true);
+        playerDirection = 'down'
     }
+    else if (gameState.cursors.space.isDown) {
+        slash()
+        // gameState.player.anims.stop()
+        // spaceBar = false;
+    }
+
     else {
-    gameState.player.anims.play('none');
+        gameState.player.anims.stop()
     }
+
 }
