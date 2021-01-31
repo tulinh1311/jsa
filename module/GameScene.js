f48166d30c13f1972680adfbf534e4f0e6ffaedf
Tu Linh <linhtunguyen.1311@gmail.com>
2021-01-31 13:40:23 +0700

    add scene

---
+++ module/GameScene.js
@@ -0,0 +1,115 @@
+import { createCharacterAnims } from "./CharacterAnims.js";
+import { createSkeletonAnims } from "./EnemyAnims.js";
+import { healthBar } from "./HealthBar.js";
+import { musicButton } from "./MusicButton.js";
+// import { preloader } from "./Preloader.js";
+// import { start } from "./Startscene.js";
+
+// gọi các biến, không xóa
+var gameState = {};
+var goLeft = false;
+var goRight = false;
+var goUp = false;
+var goDown = false;
+var ifSlash = false;
+var playerDirection;
+const ratio = Math.max(window.innerWidth / window.innerHeight, window.innerHeight / window.innerWidth)
+const DEFAULT_HEIGHT = 400 // any height you want
+const DEFAULT_WIDTH = ratio * DEFAULT_HEIGHT
+const healthpoints = 100;
+var speed = 100;
+
+function slash() {
+    if (playerDirection == 'right') {
+        // animation khi chẻm và đang hướng phải
+        gameState.player.anims.play('slash-right', true);
+    }
+    else if (playerDirection == 'left') { gameState.player.anims.play('slash-left', true); }
+    else if (playerDirection == 'up') { gameState.player.anims.play('slash-up', true); }
+    else if (playerDirection == 'down') { gameState.player.anims.play('slash-down', true); }
+}
+
+export default class GameScene extends Phaser.Scene {
+    constructor() {
+        super('game')
+    }
+
+    create() {
+        gameState.swordSound = this.sound.add('swordSound')
+        const music = this.sound.add("main_audio", { loop: true })
+        music.play()
+        const map = this.make.tilemap({ key: 'dungeon' })
+        const tileset = map.addTilesetImage('0x72_DungeonTilesetII_v1.3', 'tiles', 16, 16, 1, 2)
+        map.createLayer(0, tileset, 0, 0)
+        const wallLayer = map.createLayer(1, tileset, 0, 0)
+        // map.createLayer(2, tileset, 0, 0)
+        // map.createLayer(3, tileset, 0, 0)
+        // map.createLayer(4, tileset, 0, 0)
+
+        wallLayer.setCollisionByProperty({ collides: true })
+
+        //tạo player
+        gameState.player = this.physics.add.sprite(640, 1050, 'player', 240);
+        // sửa khung của player - tuyệt đói không xóa đi
+        gameState.player.body.setSize(gameState.player.width * 0.5, gameState.player.height * 0.5)
+        gameState.player.body.offset.y = 33
+        gameState.skeleton = this.add.sprite(600, 670, 'skeleton', 131)
+
+        createCharacterAnims(gameState.player.anims)
+        createSkeletonAnims(gameState.skeleton.anims)
+
+        // tạo bàn phím cho người chơi
+        gameState.cursors = this.input.keyboard.createCursorKeys();
+        // cho người chơi đâm vào tường chứ không xuyên qua
+        this.physics.add.collider(gameState.player, wallLayer);
+        // camera/cái nhìn sẽ đi theo người chơi
+        this.cameras.main.startFollow(gameState.player);
+        // minimap ở trên cùng
+        this.minimap = this.cameras.add(-50, -30, 200, 200).setZoom(0.05)
+        healthBar(this)
+
+        gameState.arrowUp = this.add.image(100, DEFAULT_HEIGHT - 150, 'arrowUp').setAlpha(0.7).setScale(0.7).setScrollFactor(0, 0).setInteractive().on('pointerdown', function () { goUp = true; this.alpha = 1 }).on('pointerup', function () { goUp = false; this.alpha = 0.7 });
+        gameState.arrowDown = this.add.image(100, DEFAULT_HEIGHT - 50, 'arrowDown').setAlpha(0.7).setScale(0.7).setScrollFactor(0, 0).setInteractive().on('pointerdown', function () { goDown = true; this.alpha = 1 }).on('pointerup', function () { goDown = false; this.alpha = 0.7 });
+        gameState.arrowLeft = this.add.image(50, DEFAULT_HEIGHT - 100, 'arrowLeft').setAlpha(0.7).setScale(0.7).setScrollFactor(0, 0).setInteractive().on('pointerdown', function () { goLeft = true; this.alpha = 1 }).on('pointerup', function () { goLeft = false; this.alpha = 0.7 });
+        gameState.arrowRight = this.add.image(150, DEFAULT_HEIGHT - 100, 'arrowRight').setAlpha(0.7).setScale(0.7).setScrollFactor(0, 0).setInteractive().on('pointerdown', function () { goRight = true; this.alpha = 1 }).on('pointerup', function () { goRight = false; this.alpha = 0.7 });
+        gameState.slashButton = this.add.circle(DEFAULT_WIDTH - 90, DEFAULT_HEIGHT - 90, 50, 0xffffff, 0.7).setScrollFactor(0, 0).setInteractive().on('pointerdown', function () { ifSlash = true; this.alpha = 1 }).on('pointerup', function () { ifSlash = false; this.alpha = 0.7 });
+        this.add.image(DEFAULT_WIDTH - 90, DEFAULT_HEIGHT - 90, 'gladius').setScrollFactor(0, 0).setScale(0.13)
+
+        musicButton(this, DEFAULT_HEIGHT, DEFAULT_WIDTH)
+    }
+
+    update() {
+        // gameState.cursors.{{ up/dow/left/right/space/shift }}.isDown : khi giữ phím
+        if (gameState.cursors.right.isDown || goRight) {
+            // cho nhân vật di chuyển
+            gameState.player.setVelocity(speed, 0)
+            // chơi animation khi di chuyển phải
+            gameState.player.anims.play('right', true);
+            // lưu lại để dùng khi chém
+            playerDirection = 'right'
+        }
+        else if (gameState.cursors.left.isDown || goLeft) {
+            gameState.player.setVelocity(-speed, 0)
+            gameState.player.anims.play('left', true);
+            playerDirection = 'left'
+        }
+        else if (gameState.cursors.up.isDown || goUp) {
+            gameState.player.setVelocity(0, -speed)
+            gameState.player.anims.play('up', true);
+            playerDirection = 'up'
+        }
+        else if (gameState.cursors.down.isDown || goDown) {
+            gameState.player.setVelocity(0, speed)
+            gameState.player.anims.play('down', true);
+            playerDirection = 'down'
+        }
+        else if (gameState.cursors.space.isDown || ifSlash) { slash(); gameState.swordSound.play() }
+        else {
+            // không hoạt động gì
+            gameState.player.anims.stop()
+            gameState.player.setVelocity(0, 0)
+
+        }
+        gameState.skeleton.anims.play("skeleton-thrust-right", true)
+    }
+}
--- module/Preloader.js
+++ module/Preloader.js
@@ -1,89 +1,93 @@
-const preloader = function(game) {
-    // if (game)
-    var progressBar = game.add.graphics();
-            var progressBox = game.add.graphics();
-            progressBox.fillStyle(0x222222, 0.8);
-            progressBox.fillRect(240, 270, 320, 50);
-            
-            var width = game.cameras.main.width;
-            var height = game.cameras.main.height;
-            var loadingText = game.make.text({
-                x: width / 2,
-                y: height / 2 - 50,
-                text: 'Loading...',
-                style: {
-                    font: '16px monospace',
-                    fill: '#ffffff'
-                }
-            });
-            loadingText.setOrigin(0.5, 0.5);
-            
-            var percentText = game.make.text({
-                x: width / 2,
-                y: height / 2 - 5,
-                text: '0%',
-                style: {
-                    font: '16px monospace',
-                    fill: '#ffffff'
-                }
-            });
-            percentText.setOrigin(0.5, 0.5);
-            
-            var assetText = game.make.text({
-                x: width / 2,
-                y: height / 2 + 50,
-                text: '',
-                style: {
-                    font: '16px monospace',
-                    fill: '#ffffff'
-                }
-            });
- 
-            assetText.setOrigin(0.5, 0.5);
-            
-            game.load.on('progress', function (value) {
-                percentText.setText(parseInt(value * 100) + '%');
-                progressBar.clear();
-                progressBar.fillStyle(0xffffff, 1);
-                progressBar.fillRect(250, 280, 300 * value, 30);
-            });
-            
-            game.load.on('fileprogress', function (file) {
-                assetText.setText('Loading asset: ' + file.key);
-            });
- 
-            game.load.on('complete', function () {
-                progressBar.destroy();
-                progressBox.destroy();
-                loadingText.destroy();
-                percentText.destroy();
-                assetText.destroy();
-            });
-            
-    var number = 64
-    game.load.audio('main_audio', 'https://labs.phaser.io/assets/audio/Andrea_Milana_-_Harlequin_-_The_Clockworks_-_Electribe_MX_REMIX.m4a')
-    game.load.audio('swordSound', 'https://labs.phaser.io/assets/audio/SoundEffects/sword.mp3')
-    // gạch trong dungeon
-    game.load.image('tiles', 'resource/0x72_DungeonTilesetII_v1.3.1/tileset.png');
-    // dungeon map
-    game.load.tilemapTiledJSON('dungeon', 'resource/dungeon.json')
-    game.load.tilemapTiledJSON('startBackground', 'resource/menu.json')
-    // player
-    game.load.spritesheet('player', 'resource/spritesheet.png', { frameWidth: number, frameHeight: number })
-    game.load.spritesheet('slash', 'resource/slash-sprite.png', { frameWidth: number * 3, frameHeight: number * 3 })
-    game.load.spritesheet('skeleton', 'resource/skeleton.png', { frameWidth: number, frameHeight: number })
+export default class Preloader extends Phaser.Scene {
+    constructor() {
+        super('preloader')
+    }
+    preload() {
+        var progressBar = this.add.graphics();
+        var progressBox = this.add.graphics();
+        progressBox.fillStyle(0x222222, 0.8);
+        progressBox.fillRect(240, 270, 320, 50);
 
-    game.load.image('arrowUp', 'resource/arrow/arrowUp.png')
-    game.load.image('arrowDown', 'resource/arrow/arrowDown.png')
-    game.load.image('arrowLeft', 'resource/arrow/arrowLeft.png')
-    game.load.image('arrowRight', 'resource/arrow/arrowRight.png')
+        var width = this.cameras.main.width;
+        var height = this.cameras.main.height;
+        var loadingText = this.make.text({
+            x: width / 2,
+            y: height / 2 - 50,
+            text: 'Loading...',
+            style: {
+                font: '16px monospace',
+                fill: '#ffffff'
+            }
+        });
+        loadingText.setOrigin(0.5, 0.5);
 
-    game.load.image('gladius', 'resource/gladius.png')
+        var percentText = this.make.text({
+            x: width / 2,
+            y: height / 2 - 5,
+            text: '0%',
+            style: {
+                font: '16px monospace',
+                fill: '#ffffff'
+            }
+        });
+        percentText.setOrigin(0.5, 0.5);
 
-    game.load.image('musicOn', 'resource/musicOn.png')
-    game.load.image('musicOff', 'resource/musicOff.png')
-}
+        var assetText = this.make.text({
+            x: width / 2,
+            y: height / 2 + 50,
+            text: '',
+            style: {
+                font: '16px monospace',
+                fill: '#ffffff'
+            }
+        });
 
-export {
-    preloader
+        assetText.setOrigin(0.5, 0.5);
+
+        this.load.on('progress', function (value) {
+            percentText.setText(parseInt(value * 100) + '%');
+            progressBar.clear();
+            progressBar.fillStyle(0xffffff, 1);
+            progressBar.fillRect(250, 280, 300 * value, 30);
+        });
+
+        this.load.on('fileprogress', function (file) {
+            assetText.setText('Loading asset: ' + file.key);
+        });
+
+        this.load.on('complete', function () {
+            progressBar.destroy();
+            progressBox.destroy();
+            loadingText.destroy();
+            percentText.destroy();
+            assetText.destroy();
+        });
+
+        var number = 64
+        this.load.audio('main_audio', 'https://labs.phaser.io/assets/audio/Andrea_Milana_-_Harlequin_-_The_Clockworks_-_Electribe_MX_REMIX.m4a')
+        this.load.audio('swordSound', 'https://labs.phaser.io/assets/audio/SoundEffects/sword.mp3')
+        // gạch trong dungeon
+        this.load.image('tiles', 'resource/0x72_DungeonTilesetII_v1.3.1/tileset.png');
+        // dungeon map
+        this.load.tilemapTiledJSON('dungeon', 'resource/dungeon.json')
+        this.load.tilemapTiledJSON('startBackground', 'resource/menu.json')
+        // player
+        this.load.spritesheet('player', 'resource/spritesheet.png', { frameWidth: number, frameHeight: number })
+        this.load.spritesheet('slash', 'resource/slash-sprite.png', { frameWidth: number * 3, frameHeight: number * 3 })
+        this.load.spritesheet('skeleton', 'resource/skeleton.png', { frameWidth: number, frameHeight: number })
+
+        this.load.image('arrowUp', 'resource/arrow/arrowUp.png')
+        this.load.image('arrowDown', 'resource/arrow/arrowDown.png')
+        this.load.image('arrowLeft', 'resource/arrow/arrowLeft.png')
+        this.load.image('arrowRight', 'resource/arrow/arrowRight.png')
+
+        this.load.image('gladius', 'resource/gladius.png')
+
+        this.load.image('musicOn', 'resource/musicOn.png')
+        this.load.image('musicOff', 'resource/musicOff.png')
+    }
+
+    create() {
+        this.scene.start('game')
+    }
 }
